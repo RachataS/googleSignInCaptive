ef3efe6ec256deedcfb3b9dbf8c3034aae3bb19f
RachataS <64030161@kmitl.ac.th>
2023-06-08 16:53:51 +0700

    install crawler

--- GoogleCaptive/app/Http/Controllers/GoogleController.php
+++ GoogleCaptive/app/Http/Controllers/GoogleController.php
@@ -8,45 +8,50 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Exception;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\DomParser;
 
 class GoogleController extends Controller
 {
-    public function googlepage(){
+    public function googlepage()
+    {
         $googlelogin = socialite::driver('google')->redirect();
         $redirectUrl = $googlelogin->getTargetUrl();
         info($redirectUrl);
         //return response()->json($redirectUrl);
 
         $urlresponse = Http::get($redirectUrl);
-        if($urlresponse->ok()){
-            info($urlresponse);
+        if ($urlresponse->ok()) {
+            $html = $urlresponse->body();
+            $dom = new DomParser($html);
+
+            info($dom);
             return $urlresponse;
         }
         return redirect($redirectUrl);
     }
 
-    public function googlecallback(){
-        try{
+    public function googlecallback()
+    {
+        try {
             $user = Socialite::driver('google')->user();
-            $finduser = User::where('google_id',$user->id)->first();
+            $finduser = User::where('google_id', $user->id)->first();
 
-            if($finduser){
+            if ($finduser) {
                 Auth::login($finduser);
                 return redirect()->intended('dashboard');
-            }
-            else{
+            } else {
                 $newUser = User::create([
                     'name' => $user->name,
-                    'email'=> $user->email,
+                    'email' => $user->email,
                     'google_id' => $user->id,
-                    'password'=> encrypt("12345"),
+                    'password' => encrypt("12345"),
                 ]);
                 Auth::login($newUser);
 
                 return redirect()->intended('dashboard');
             }
-        }catch(Exception $e){
+        } catch (Exception $e) {
             dd($e->getMessage());
-}
+        }
     }
 }
